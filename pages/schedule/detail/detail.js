f86399921c69f5151d3c8c154bcc40b5ffb21d88
wufajin <30524319+wufajin@users.noreply.github.com>
2017-09-24 23:43:11 +0800

    接受邀请时判断openId是否为空，为空则重新查询

--- pages/schedule/detail/detail.js
+++ pages/schedule/detail/detail.js
@@ -1,12 +1,12 @@
 let app = getApp()
-import { joinSchedule, queryTeamMember, queryScheduleById} from "../../../service/service.js" 
+import { joinSchedule, queryTeamMember, queryScheduleById, getOpenId } from "../../../service/service.js"
 Page({
   data: {
     isHidden: false,
     jhDataForTabbar: [
       {
-        sIconUrl: "../../../images/base/schedule-invite-selected.png", 
-        sTitle: "邀请同事",                    
+        sIconUrl: "../../../images/base/schedule-invite-selected.png",
+        sTitle: "邀请同事",
         event: "invite",
         openType: "share"
       },
@@ -32,12 +32,12 @@ Page({
         jhDataForTabbar: [
           {
             sIconUrl: "../../../images/base/acceptInvite.png",
-            sTitle: "接受邀请",                    
+            sTitle: "接受邀请",
             event: "acceptInvite"
           },
           {
             sIconUrl: "../../../images/base/noAccpetInvite.png",
-            sTitle: "拒绝邀请",                  
+            sTitle: "拒绝邀请",
             event: "notAcceptInvite"
           }
         ]
@@ -45,26 +45,26 @@ Page({
 
     }
     if (options.isHidden == "true") {
-        this.setData({
-          isHidden: true
-        })
-        wx.showToast({
-          title: '已加入会议',
-        })
+      this.setData({
+        isHidden: true
+      })
+      wx.showToast({
+        title: '已加入会议',
+      })
 
     }
   },
-  onShow:function(){
+  onShow: function () {
 
   },
   modify: function () {
     var that = this;
-    if (app.globalData.openId != that.data.currentscheduleDetail.creatorOpenId){
-     wx.showModal({
-       title: '无权限编辑',
-       content: '只有创建者' + that.data.currentscheduleDetail.creatorNickName +'能编辑',
-     })
-    }else{
+    if (app.globalData.openId != that.data.currentscheduleDetail.creatorOpenId) {
+      wx.showModal({
+        title: '无权限编辑',
+        content: '只有创建者' + that.data.currentscheduleDetail.creatorNickName + '能编辑',
+      })
+    } else {
       wx.navigateTo({
         url: '../modify/modify?currentschedule=' + that.data.currentschedule,
         success: function (res) { },
@@ -72,27 +72,56 @@ Page({
         complete: function (res) { },
       })
     }
-    
+
   },
   acceptInvite: function (e) {
     var that = this
-    if(app.globalData.member==null){
-      app.globalData.teamId = that.data.currentscheduleDetail.meetingRoom.teamId
-      queryTeamMember(app.globalData.url, app, function (member) {
-        console.log("回调函数")
-        if (app.globalData.member != null) {
-          joinSchedule(app.globalData.url, app.globalData.userInfo, that.data.currentscheduleDetail, app.globalData.openId, app.globalData.token, e.detail.formId)
-        }
-        else {
-          wx.navigateTo({
-            url: '../../regist/regist?inviteBand=y',
-          })
-        }
-      })
-    }else{
-      joinSchedule(app.globalData.url, app.globalData.userInfo, that.data.currentscheduleDetail, app.globalData.openId, app.globalData.token, e.detail.formId)
+    app.globalData.teamId = that.data.currentscheduleDetail.meetingRoom.teamId
+    if (app.globalData.member == null) {
+      if (app.globalData.openId == '') {
+        wx.showLoading({
+          title: '加载中',
+        })
+        wx.login({
+          success: function (res) {
+            getOpenId(app, res.code, function (openId) {
+              app.globalData.openId = openId
+              wx.hideLoading()
+              queryTeamMember(app.globalData.url, app, function (member) {
+                console.log("回调函数")
+                if (app.globalData.member != null) {
+                  wx.hideLoading()
+                  joinSchedule(app.globalData.url, app.globalData.userInfo, that.data.currentscheduleDetail, app.globalData.openId, app.globalData.token, e.detail.formId)
+                }
+                else {
+                  wx.hideLoading()
+                  wx.navigateTo({
+                    url: '../../regist/regist?inviteBand=y',
+                  })
+                }
+              })
+            })
+          }
+        })
+      }
+      else {
+        console.log('called2========================')
+        queryTeamMember(app.globalData.url, app, function (member) {
+          if (app.globalData.member != null) {
+            joinSchedule(app.globalData.url, app.globalData.userInfo, that.data.currentscheduleDetail, app.globalData.openId, app.globalData.token, e.detail.formId)
+          }
+          else {
+            wx.navigateTo({
+              url: '../../regist/regist?inviteBand=y',
+            })
+          }
+        })
+      }
+    } else {
+      console.log('called=========================')
+      joinSchedule(app.globalData.url, app.globalData.userInfo, that.data.currentscheduleDetail, app.globalData.member.openId, app.globalData.token, e.detail.formId)
     }
-    
+
   },
   notAcceptInvite: function () {
     wx.navigateBack({
